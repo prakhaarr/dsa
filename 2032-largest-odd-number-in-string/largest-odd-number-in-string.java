7bf361e91f1004e7b1a8b9916cc62ce268b7e36b
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-03-27 01:06:22 +0530

    Time: 1 ms (100.00%) | Memory: 45.1 MB (83.39%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
@@ -5,7 +5,7 @@ class Solution {
         
         for (int i = n - 1; i >= 0; i--) {
             if ((num.charAt(i)) % 2 != 0) {
-                return num.substring(0, i + 1);
+                return num.substring(0,i + 1);
             }
         }
         
