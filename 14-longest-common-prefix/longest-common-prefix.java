4666cc0092cdf4ca0e69a085ff9e0b57dfca677b
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-03-27 14:07:09 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (85.32%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -4,7 +4,7 @@ class Solution {
             return "";
         }
         
-        String prefix = strs[0]; // Initialize prefix with the first string
+        String prefix = strs[0];
         
         for (int i = 1; i < strs.length; i++) {
             while (strs[i].indexOf(prefix) != 0) { // Keep removing characters from prefix until it matches
