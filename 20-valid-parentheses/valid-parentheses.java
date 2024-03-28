d4fbaf7b55031688cc5789aa712c9bed1e3b640d
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-03-28 21:35:36 +0530

    Time: 1 ms (98.65%) | Memory: 41.5 MB (45.69%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) {
+                    return false; // There is a closing bracket without a corresponding opening bracket
+                }
+                char top = stack.pop();
+                if ((ch == ')' && top != '(') || (ch == ']' && top != '[') || (ch == '}' && top != '{')) {
+                    return false; // Mismatched brackets
+                }
+            }
+        }
+
+        return stack.isEmpty(); // Stack should be empty if all opening brackets are closed properly
+    }
+}
