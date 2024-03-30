e456e4b943d5055d5dc1980bd8f630665e3c21e2
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-03-31 01:08:21 +0530

    Time: 4 ms (99.30%) | Memory: 46.9 MB (64.09%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -1,29 +1,20 @@
-class Solution {
-    public int subarraysWithKDistinct(int[] nums, int k) {
-        int subWithMaxK = subarrayWithAtMostK(nums, k);
-        int reducedSubWithMaxK = subarrayWithAtMostK(nums, k - 1);
-        return subWithMaxK - reducedSubWithMaxK;
-    }
-    
-    public int subarrayWithAtMostK(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int left = 0, right = 0, ans = 0;
-        
-        while (right < nums.length) {
-            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
-            
-            while (map.size() > k) {
-                map.put(nums[left], map.get(nums[left]) - 1);
-                if (map.get(nums[left]) == 0) {
-                    map.remove(nums[left]);
-                }
-                left++;
+ class Solution {
+     public int subarraysWithKDistinct(int[] nums, int K) {
+        int[] numFreq = new int[nums.length+1];
+        int distinct = 0, start = 0, minEnd = 0, total = 0;
+        while (distinct == K || minEnd < nums.length) {
+            while (distinct < K && minEnd < nums.length) 
+			    if (numFreq[nums[minEnd++]]++ == 0) 
+                distinct++;
+            int maxEnd = minEnd;
+            while (maxEnd < nums.length && numFreq[nums[maxEnd]] > 0)
+			    maxEnd++;
+            while (distinct == K) {
+                if (numFreq[nums[start++]]-- == 1)
+                distinct--;
+                total += (maxEnd - minEnd + 1);
             }
-            
-            ans += right - left + 1; // Size of subarray
-            right++;
         }
-        
-        return ans;
+        return total;
     }
-}
+ }
