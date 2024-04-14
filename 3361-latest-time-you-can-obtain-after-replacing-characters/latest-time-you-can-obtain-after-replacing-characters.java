e4d0e2de2c52a13f80e4c5962f8c9e655514db74
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-04-14 10:52:56 +0530

    Time: 1 ms (100.00%) | Memory: 42.9 MB (50.00%) - LeetSync

---
+++ 3361-latest-time-you-can-obtain-after-replacing-characters/latest-time-you-can-obtain-after-replacing-characters.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String findLatestTime(String s) {
+        char[] conv = s.toCharArray();
+        int i=1;
+        while(i!=5){
+
+        if(conv[0]=='?' && conv[1]=='?'){
+            conv[0]='1';
+            conv[1]='1';
+        }    
+        if(conv[0]=='?'){
+            conv[0] = '1';
+            if(conv[0]=='1' && conv[1] >='2' ){
+                conv[0]= '0';
+            }
+        }
+        else if(conv[0]=='1' && conv[1]=='?'){
+            conv[1] = '1';
+        }
+        else if(conv[0]=='0' && conv[1]=='?'){
+            conv[1] = '9';
+        }
+        if(conv[3]=='?'){
+            conv[3] = '5';
+        }
+        else if(conv[4]=='?'){
+            conv[4]='9';
+        }
+        i++;
+        }
+
+        
+        String str = new String(conv);
+        return str;
+    }
+}
