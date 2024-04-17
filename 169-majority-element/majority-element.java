f01305880d81bed9df60f1df5aff46642ef01d05
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-04-17 22:08:13 +0530

    Time: 4 ms (55.05%) | Memory: 52.6 MB (66.73%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,23 +1,33 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap <Integer,Integer> mp = new HashMap<>();
+    //     HashMap <Integer,Integer> mp = new HashMap<>();
 
-        for(int i =0;i<nums.length;i++){
-            mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
-        }
-        
-        int maxKey = 0;
-        int maxFrequency = 0;
+    //     for(int i =0;i<nums.length;i++){
+    //         mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
+    //     }
 
-        for (Map.Entry<Integer, Integer> entry : mp.entrySet()) {
-            int key = entry.getKey();
-            int frequency = entry.getValue();
-            if (frequency > maxFrequency) {
-                maxFrequency = frequency;
-                maxKey = key;
-            }
-        }
+    //     int maxKey = 0;
+    //     int maxFrequency = 0;
 
-        return maxKey;
+    //     for (Map.Entry<Integer, Integer> entry : mp.entrySet()) {
+    //         int key = entry.getKey();
+    //         int frequency = entry.getValue();
+    //         if (frequency > maxFrequency) {
+    //             maxFrequency = frequency;
+    //             maxKey = key;
+    //         }
+    //     }
+
+    //     return maxKey;
+    // }
+
+    Arrays.sort(nums);
+
+    return nums[nums.length/2];
+    
+
+
+
+
     }
 }
