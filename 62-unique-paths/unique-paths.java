e24b95b4e57b2338e7864c0bdf27dfa8a0528b49
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-04-21 23:58:36 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (69.81%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -2,7 +2,7 @@ class Solution {
     int[][] t;
 
     public int solve(int m, int n, int i, int j) {
-        if(i >= m || i < 0 || j >= n || j < 0)
+        if(i >= m || j >= n)
             return 0;
            
         if(i == m-1 && j == n-1)
