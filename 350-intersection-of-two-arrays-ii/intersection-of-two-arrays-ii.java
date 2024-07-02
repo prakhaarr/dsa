1dc53d1ccf34698ed50e0a5cd2feba07d7c21f11
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-07-03 01:29:38 +0530

    Time: 3 ms (91.02%) | Memory: 43.2 MB (38.89%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+
+        // Fill the map with the count of each element in nums1
+        for (int i = 0; i < nums1.length; i++) {
+            int num = nums1[i];
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        // Iterate through nums2 and check if the element exists in the map with a non-zero count
+        for (int i = 0; i < nums2.length; i++) {
+            int num = nums2[i];
+            if (map.containsKey(num) && map.get(num) > 0) {
+                result.add(num);
+                map.put(num, map.get(num) - 1);
+            }
+        }
+
+        // Convert the result list to an array
+        int[] intersection = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            intersection[i] = result.get(i);
+        }
+
+        return intersection;
+    }
+}
