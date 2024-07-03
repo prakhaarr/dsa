2484c2955de055594dfea8837f20e9f112f7722d
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-07-04 02:04:00 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (82.70%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.java
+++ 2050-count-good-numbers/count-good-numbers.java
@@ -1,17 +1,38 @@
-class Solution {
-    int modulo= 1000000007;
-    public int countGoodNumbers(long n) {
+// class Solution {
+//     int modulo= 1000000007;
+//     public int countGoodNumbers(long n) {
         
-      return (int)((rec(n/2,4)*rec((n+1)/2,5))%modulo);
-    }
-    long rec(long n, int x)
-    {
+//       return (int)((rec(n/2,4)*rec((n+1)/2,5))%modulo);
+//     }
+//     long rec(long n, int x)
+//     {
        
-        if(n==0)
-            return 1;
-        long num = rec(n/2,x);
-        return (num*num)*(n%2==0?1:x)%modulo;
+//         if(n==0)
+//             return 1;
+//         long num = rec(n/2,x);
+//         return (num*num)*(n%2==0?1:x)%modulo;
         
-    }
+//     }
 
-}
+// }
+
+class Solution {
+    private long MOD = 1000000007;
+    public int countGoodNumbers(long n) {
+        long even = (n+1)/2;
+        long odd = n/2;
+        long first = pow(5,even)%MOD;
+        long second = pow(4,odd)%MOD;
+        return (int)((first*second)%MOD);
+    }                     
+    private long pow(long x,long n)
+    {
+        if(n==0)return 1;
+        long temp = pow(x,n/2);
+        if(n%2==0)
+        {
+            return (temp*temp)%MOD;
+        }        
+        return (x*temp*temp)%MOD;
+        }
+    }
