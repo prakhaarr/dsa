60509653a6c3d61ad215f9c3d62b076d66bb554c
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-07-04 02:04:21 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (53.72%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.java
+++ 2050-count-good-numbers/count-good-numbers.java
@@ -1,21 +1,3 @@
-// class Solution {
-//     int modulo= 1000000007;
-//     public int countGoodNumbers(long n) {
-        
-//       return (int)((rec(n/2,4)*rec((n+1)/2,5))%modulo);
-//     }
-//     long rec(long n, int x)
-//     {
-       
-//         if(n==0)
-//             return 1;
-//         long num = rec(n/2,x);
-//         return (num*num)*(n%2==0?1:x)%modulo;
-        
-//     }
-
-// }
-
 class Solution {
     private long MOD = 1000000007;
     public int countGoodNumbers(long n) {
