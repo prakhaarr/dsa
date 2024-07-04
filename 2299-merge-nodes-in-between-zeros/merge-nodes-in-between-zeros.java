516e1e0724deb7b9ab5c5f97de914fc2b679e49e
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-07-05 02:54:50 +0530

    Time: 4 ms (100.00%) | Memory: 79.5 MB (90.29%) - LeetSync

---
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.java
@@ -0,0 +1,93 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeNodes(ListNode head) {
+        ListNode originalHead = head;
+        ListNode curr = head.next;
+        int sum =0;
+
+        while(curr!=null){
+            if(curr.val==0){
+                head.val = sum;
+                sum =0;
+                if(curr.next !=null){
+                    head = head.next;
+                } 
+            }
+            sum += curr.val;
+            curr = curr.next;
+        }
+
+        head.next = null;
+        return originalHead;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // ListNode temp = head;
+        // List<Integer> ls = new ArrayList<>();
+        // List<Integer> ans = new ArrayList<>();
+        // while(temp!=null){
+        //     ls.add(temp.val);
+        //     temp =temp.next;
+        // }
+
+        // for(int i=1;i<ls.size();i++){
+        //     int sum =0;
+        //     if(ls.get(i)!=0 && ls.get(i-1)==0){
+        //         sum+=ls.get(i);
+        //     }
+        //     for(int j = i+1;j<ls.size();j++){
+        //         if(ls.get(i) !=0 && ls.get(j) !=0 && sum != 0){
+        //             sum+=ls.get(j);
+        //         }
+        //         else{
+        //             if(ls.get(j)==0){
+        //                 break;
+        //             }
+        //         }
+        //     }
+        //     if(sum!=0){
+        //             ans.add(sum);
+        //     }
+            
+        // }
+        // //System.out.print(ans);
+
+        // ListNode result = new ListNode(0);
+        // ListNode op = result;
+
+        // for(int i=0;i<ans.size();i++){
+        //     op.next = new ListNode(ans.get(i));
+        //     op = op.next;
+        // }
+
+
+
+        // return result.next;
+
+
+
+        
+    }
+}
