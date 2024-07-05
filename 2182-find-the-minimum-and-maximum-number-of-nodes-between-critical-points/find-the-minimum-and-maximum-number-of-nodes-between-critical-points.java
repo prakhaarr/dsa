148485d1497f6b2b262274cab70d32ad4886b64e
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-07-06 03:35:25 +0530

    Time: 10 ms (26.23%) | Memory: 69.9 MB (6.92%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -9,7 +9,40 @@
  * }
  */
 
+ // class Solution {
+//     public int[] nodesBetweenCriticalPoints(ListNode head) {
+//         ListNode temp = head;
+//         if(temp.next.next == null){
+//             int[] emp = new int[2];
+//             emp[0]= -1;
+//             emp[1] = -1;
+//             return emp;
+//         }
+//         int count =1;
+//         ListNode curr = head.next;
+//         ListNode prev = head;
+//         List<Integer> ls = new ArrayList<>();
+//         //prev.next = head;
+//         while(curr!=null && curr.next !=null){
+//             if((curr.val<curr.next.val && curr.val<prev.val) || (curr.val>curr.next.val && curr.val>prev.val)){
+//                 ls.add(curr.val);
+//                 prev = prev.next;
+//                 curr = curr.next;
+//                 count++;
+//             }
+//             prev = prev.next;
+//             curr = curr.next;
+//             count++;
+//         }
 
+//         Collections.sort(ls);
+//         int[] ans = new int[2];
+//         ans[0] = ls.get(0);
+//         ans[1] = ls.get(1);
+
+//         return ans;
+//     }
+// }
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
         if (head == null || head.next == null || head.next.next == null) {
