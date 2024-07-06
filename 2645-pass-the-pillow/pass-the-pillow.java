4f21f3f5f68845d2ed26f5046f10bf8f5c15e206
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-07-07 03:03:48 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (6.01%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.java
+++ 2645-pass-the-pillow/pass-the-pillow.java
@@ -4,13 +4,13 @@ class Solution {
             return time+1;
         }
 
-        int rounds = time/(n-1);
-        int temp = time % (n-1);
+        int rounds = time/(n-1); // shows no. of rounds
+        int temp = time % (n-1); // shows at which position
 
-        if(rounds%2==1){
+        if(rounds%2==1){ // if rounds are even, i.e it will start temp from begining anf id odd it will start from back
             return n-temp;
         }
-        return temp+1;
+        return temp+1; //anf id odd it will start from back
 
         
     }
