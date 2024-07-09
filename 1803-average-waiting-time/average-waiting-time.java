796a41ed25c025682712067e455649eecdad2665
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-07-10 02:19:56 +0530

    Time: 7 ms (9.09%) | Memory: 82.9 MB (7.11%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.java
+++ 1803-average-waiting-time/average-waiting-time.java
@@ -3,14 +3,19 @@ class Solution {
 
         List<Integer> ls = new ArrayList<>();
 
+        int timeTaken =0;
         int time = customers[0][0] + customers[0][1];
         ls.add(time-customers[0][0]);
+        //timeTaken = time-customers[0][0];
+
 
         for(int i =1;i<customers.length;i++){
             time = Math.max(time + customers[i][1],customers[i][0]+customers[i][1]);
             int ans = time - customers[i][0];
             ls.add(ans);
+            //timeTaken += ans;
         }
+        // double finall = timeTaken/customers.length;
         double finall = 0;
         for(int i=0;i<ls.size();i++){
             finall+=ls.get(i);
