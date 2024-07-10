dbed7f817f12a00008b45ed318e113a5ab1bcb55
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-07-11 01:23:36 +0530

    Time: 1 ms (99.42%) | Memory: 42.2 MB (19.93%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        int count = 0;
+        for(int i =0;i<logs.length;i++){
+            if(logs[i].equals("../")){
+                if(count>0){
+                    count--;
+                }
+            }
+            else if(!logs[i].equals("./")){
+                count++;
+            }
+        }
+
+        return count;
+
+    }
+}
