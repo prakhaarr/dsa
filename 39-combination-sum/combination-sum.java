6f03d07ab8fc0e7c175818ec0a07e6fb9726ea37
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-07-17 02:14:47 +0530

    Time: 1 ms (99.90%) | Memory: 44.3 MB (85.99%) - LeetSync

---
+++ 39-combination-sum/combination-sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    private void findComb(int index, int[] arr, int target, List<List<Integer>>ans,List<Integer> ds){
+        if(index==arr.length){
+            if(target == 0){
+                ans.add(new ArrayList<>(ds));
+            }
+            return;
+        }
+
+        //not taking that element, means same index
+        if(arr[index]<=target){
+            ds.add(arr[index]);
+            findComb(index,arr,target-arr[index],ans,ds);
+            ds.remove(ds.size()-1);
+
+        }
+        // moving to new index
+        findComb(index+1,arr,target,ans,ds);
+    }
+
+
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        findComb(0,candidates,target,ans,new ArrayList<>());
+
+        return ans;
+    }
+}
