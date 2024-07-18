20b8d9b2e5ce2ec2d52262afa9f6f67d9f02551a
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-07-18 21:52:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (23.60%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,13 +1,51 @@
 class Solution {
-    public void sortColors(int[] nums) {
-        for(int i=nums.length-1;i>=0;i--){
-            for(int j=0;j<i;j++){
-                if(nums[j]>nums[j+1]){
-                    int temp=nums[j];
-                    nums[j]=nums[j+1];
-                    nums[j+1]=temp;
-                }
+
+    public static void sortt(int[] nums, int low,int high){
+        if(low>=high){
+            return;
+        }
+
+        int s = low;
+        int e = high;
+        int mid = s + (e-s)/2;
+        int pivot  = nums[mid];
+
+        while(s<=e){
+            while(nums[s] < pivot){
+                s++;
+
+            }
+            while(nums[e] > pivot){
+                e--;
+            }
+            if(s<=e){
+                int temp = nums[s];
+                nums[s] = nums[e];
+                nums[e] = temp;
+                s++;
+                e--;
             }
         }
+        sortt(nums,low,e);
+        sortt(nums,s,high);
+        
+    }
+
+
+
+    public void sortColors(int[] nums) {
+        // for(int i=nums.length-1;i>=0;i--){
+        //     for(int j=0;j<i;j++){
+        //         if(nums[j]>nums[j+1]){
+        //             int temp=nums[j];
+        //             nums[j]=nums[j+1];
+        //             nums[j+1]=temp;
+        //         }
+        //     }
+        // }
+
+        sortt(nums,0,nums.length-1);
+
+
     }
 }
