7d64e5cbc494e91716f1be1cdb113806ed5c771b
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-07-19 19:20:27 +0530

    Time: 1 ms (99.92%) | Memory: 43.3 MB (69.19%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,31 +1,5 @@
 
 class Solution {
-    // void helper(int nums[],int i,List<Integer> subset,List<List<Integer>> ans){
-    //     if(i == nums.length){
-    //         ans.add(new ArrayList<>(subset));
-    //         return ;
-    //     }
-        
-    //      subset.add(nums[i]);
-        
-    //     // We ask recursion to do rest of the task
-        
-    //     helper(nums, i + 1, subset, ans);
-        
-    //     // Backtrack and Undo the change we have done
-        
-    //     subset.remove(subset.size() - 1);
-        
-    //     // While using Don't Pick option, we must ensure we skip all the Duplicate Occurrences of nums[i]
-    
-    //     while(i < nums.length - 1 && (nums[i] == nums[i + 1] ) )
-    //     {
-    //         i++ ;
-    //     }
-    //     helper(nums, i + 1, subset, ans);
-    //     return ;
-    // }
-
     public static void helper(int index, int[] nums, List<Integer> ds, List<List<Integer>> ans, int len){
         if(index==len){
             ans.add(new ArrayList<>(ds));
@@ -44,8 +18,6 @@ class Solution {
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        //List<Integer> ds = new ArrayList<>();
-
         Arrays.sort(nums);
         helper(0,nums, new ArrayList<>(), ans,nums.length);
         return ans;
