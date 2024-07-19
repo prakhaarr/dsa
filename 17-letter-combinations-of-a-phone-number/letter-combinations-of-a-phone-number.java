b6f92e44fc4b9efece691a47f40f5098e788d3a3
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-07-19 20:18:09 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (74.16%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,74 +1,4 @@
 class Solution {
-    // public List<String> letterCombinations(String digits) {
-    //     List<String> ls = new ArrayList<>();
-
-    //     for(int i =0;i<digits.length();i++){
-    //         if(digits.charAt(i)==2){
-    //             ls.add("a");
-    //             ls.add("a"+1);
-    //             ls.add("a"+2);
-    //         }
-    //         else if(digits.charAt(i)==3){
-    //             ls.add("a"+3);
-    //             ls.add("a"+4);
-    //             ls.add("a"+5);
-    //         }
-    //         else if(digits.charAt(i)==4){
-    //             ls.add("a"+6);
-    //             ls.add("a"+7);
-    //             ls.add("a"+8);
-    //         }
-    //         else if(digits.charAt(i)==5){
-    //             ls.add("a"+9);
-    //             ls.add("a"+10);
-    //             ls.add("a"+11);
-    //         }
-    //         else if(digits.charAt(i)==6){
-    //             ls.add("a"+12);
-    //             ls.add("a"+13);
-    //             ls.add("a"+14);
-    //         }
-    //         else if(digits.charAt(i)==7){
-    //             ls.add("a"+15);
-    //             ls.add("a"+16);
-    //             ls.add("a"+17);
-    //         }
-    //         else if(digits.charAt(i)==8){
-    //             ls.add("a"+18);
-    //             ls.add("a"+19);
-    //             ls.add("a"+20);
-    //         }
-    //         else if(digits.charAt(i)==9){
-    //             ls.add("a"+21);
-    //             ls.add("a"+22);
-    //             ls.add("a"+23);
-    //         }
-    //     }
-
-    //     List<String> ans = new ArrayList<>();
-    //     StringBuilder hehe = new StringBuilder();
-
-    //     helper(0,hehe,ls,ans,digits.length());
-
-    //     return ans;
-        
-    // }
-
-    // public static void helper(int index, StringBuilder curr, List<String> ls, List<String> ans,int len){
-    //     if(index==ls.size()){
-    //         if(curr.length()==len){
-    //             ans.add(curr.toString());
-    //         }
-    //     }
-
-    //     curr.append(ls.get(index));
-    //     helper(index+1,curr,ls,ans,len);
-    //     curr.setLength(curr.length()-1);
-    //     helper(index+1,curr,ls,ans,len);
-
-
-    // }
-    
     private static final String[] KEYPAD = {
         "",     // 0
         "",     // 1
@@ -88,22 +18,22 @@ class Solution {
             return ans;
         }
 
-        StringBuilder currentCombination = new StringBuilder();
-        helper(digits, 0, currentCombination, ans);
+        StringBuilder curr = new StringBuilder();
+        helper(digits, 0, curr, ans);
         return ans;
     }
 
-    private void helper(String digits, int index, StringBuilder currentCombination, List<String> ans) {
+    private void helper(String digits, int index, StringBuilder curr, List<String> ans) {
         if (index == digits.length()) {
-            ans.add(currentCombination.toString());
+            ans.add(curr.toString());
             return;
         }
 
         String letters = KEYPAD[digits.charAt(index) - '0'];
         for (char letter : letters.toCharArray()) {
-            currentCombination.append(letter);
-            helper(digits, index + 1, currentCombination, ans);
-            currentCombination.setLength(currentCombination.length() - 1);  // backtrack
+            curr.append(letter);
+            helper(digits, index + 1, curr, ans);
+            curr.setLength(curr.length() - 1);  // backtrack
         }
     }
   
