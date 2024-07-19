d19587733cc7c7cc1acd4f0ce9bf66faf04223d8
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-07-19 19:19:57 +0530

    Time: 1 ms (99.92%) | Memory: 43.2 MB (69.19%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,37 +1,53 @@
 
 class Solution {
-    void helper(int nums[],int i,List<Integer> subset,List<List<Integer>> ans){
-        if(i == nums.length){
-            ans.add(new ArrayList<>(subset));
-            return ;
-        }
+    // void helper(int nums[],int i,List<Integer> subset,List<List<Integer>> ans){
+    //     if(i == nums.length){
+    //         ans.add(new ArrayList<>(subset));
+    //         return ;
+    //     }
         
-         subset.add(nums[i]);
+    //      subset.add(nums[i]);
         
-        // We ask recursion to do rest of the task
+    //     // We ask recursion to do rest of the task
         
-        helper(nums, i + 1, subset, ans);
+    //     helper(nums, i + 1, subset, ans);
         
-        // Backtrack and Undo the change we have done
+    //     // Backtrack and Undo the change we have done
         
-        subset.remove(subset.size() - 1);
+    //     subset.remove(subset.size() - 1);
         
-        // While using Don't Pick option, we must ensure we skip all the Duplicate Occurrences of nums[i]
+    //     // While using Don't Pick option, we must ensure we skip all the Duplicate Occurrences of nums[i]
     
-        while(i < nums.length - 1 && (nums[i] == nums[i + 1] ) )
-        {
-            i++ ;
+    //     while(i < nums.length - 1 && (nums[i] == nums[i + 1] ) )
+    //     {
+    //         i++ ;
+    //     }
+    //     helper(nums, i + 1, subset, ans);
+    //     return ;
+    // }
+
+    public static void helper(int index, int[] nums, List<Integer> ds, List<List<Integer>> ans, int len){
+        if(index==len){
+            ans.add(new ArrayList<>(ds));
+            return;
         }
-        helper(nums, i + 1, subset, ans);
-        return ;
+        ds.add(nums[index]);
+        helper(index+1,nums,ds,ans,len);
+        ds.remove(ds.size()-1);
+
+        while(index + 1 < nums.length && nums[index] == nums[index + 1]){
+            index++;
+        }
+        helper(index+1,nums,ds,ans,len);
     }
+
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> t = new ArrayList<>();
+        //List<Integer> ds = new ArrayList<>();
 
         Arrays.sort(nums);
-        helper(nums,0, t, ans);
+        helper(0,nums, new ArrayList<>(), ans,nums.length);
         return ans;
     }
 }
