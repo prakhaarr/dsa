906d2a4790976876025202bbcbea9166f804eec7
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-07-21 15:27:07 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (30.50%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,20 +1,5 @@
 class Solution {
-    //int[] dp = new int[1001];
-    // private int helper(int[] cost,int index){
-    //     // if(index >= cost.length){
-    //     //     return 0;
-    //     // }
-    //     if(n==2{
-    //         return Math.min(cost[0],cost[1]);
-    //     }
-
     
-    //     int a = cost[index] + helper(cost,index+1);
-    //     int b = cost[index] + helper(cost,index+2);
-
-    //     return Math.min(a,b);
-
-    // }
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
         
