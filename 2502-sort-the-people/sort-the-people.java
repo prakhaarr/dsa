90b14ca282ad625e6d079af949f356e285497e10
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-07-22 12:00:32 +0530

    Time: 7 ms (90.64%) | Memory: 45.2 MB (50.36%) - LeetSync

--- 2502-sort-the-people/sort-the-people.java
+++ 2502-sort-the-people/sort-the-people.java
@@ -1,16 +1,16 @@
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
-        HashMap<Integer,String> mp = new HashMap<>();
+        Map<Integer,String> mp = new TreeMap<>();
 
         for(int i=0;i<names.length;i++){
             mp.put(heights[i],names[i]);
         }
-
-        TreeMap<Integer, String> sortMP = new TreeMap<>(Comparator.reverseOrder());
-        sortMP.putAll(mp);
-
-
-        String[] ans = sortMP.values().toArray(new String[0]);
+        String[] ans = new String[names.length];
+        
+        int j = ans.length-1;
+        for(Map.Entry<Integer,String> m : mp.entrySet()){
+            ans[j--] = m.getValue();
+        }
 
 
         return ans;
