ce00323772fb9d7aa085be505516c817f67abbb8
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-07-27 16:04:49 +0530

    Time: 11 ms (97.91%) | Memory: 57.3 MB (20.45%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,51 +1,32 @@
 class Solution {
-
-    static boolean subsetSumUtil(int ind, int target, int arr[], int[][] dp) {
-        // If the target sum is 0, we have found a valid subset
-        if (target == 0)
-            return true;
-
-        // If we have processed all elements in the array
-        if (ind == 0)
-            return arr[0] == target;
-
-        // If this subproblem has already been solved, return the result
-        if (dp[ind][target] != -1)
-            return dp[ind][target] == 0 ? false : true;
-
-        // Try not taking the current element into the subset
-        boolean notTaken = subsetSumUtil(ind - 1, target, arr, dp);
-
-        // Try taking the current element into the subset if it doesn't exceed the target
-        boolean taken = false;
-        if (arr[ind] <= target)
-            taken = subsetSumUtil(ind - 1, target - arr[ind], arr, dp);
-
-        // Memoize the result and return true if either choice results in a valid subset
-        dp[ind][target] = notTaken || taken ? 1 : 0;
-        return notTaken || taken;
+    public static boolean solve(int ind,int[] arr,int target,Boolean[][] memo){
+		if(target==0){
+			return true;
+		}
+		if(ind==0){
+			return arr[0]==target;
+		}
+		if(memo[ind][target]!=null) return memo[ind][target];
+		if(arr[ind]>target){
+		memo[ind][target]=solve(ind-1,arr,target,memo);
+		return memo[ind][target];
+		}
+		memo[ind][target]=solve(ind-1,arr,target-arr[ind],memo)||solve(ind-1,arr,target,memo);
+		return memo[ind][target];
+	}
+    
+    public boolean canPartition(int[] nums) {
+        int n=nums.length;
+        int sum=0;
+		for(int i=0;i<n;i++){
+			sum+=nums[i];
+		}
+		if(sum%2!=0){
+			return false;
+		}
+		int target=sum/2;
+		Boolean [][]memo=new Boolean[n+1][target+1];
+		return solve(n-1,nums,target,memo);
     }
-
-    public boolean canPartition(int[] arr) {
-        // Calculate the total sum of the array elements
-        int n = arr.length;
-        int totSum = 0;
-        for (int i = 0; i < n; i++) {
-            totSum += arr[i];
-        }
-
-        // If the total sum is odd, it cannot be partitioned into equal subsets
-        if (totSum % 2 == 1)
-            return false;
-        else {
-            // Calculate the target sum for each subset
-            int k = totSum / 2;
-            // Create a memoization table
-            int dp[][] = new int[n][k + 1];
-            for (int row[] : dp)
-                Arrays.fill(row, -1);
-            // Call the helper function to check if a valid subset exists
-            return subsetSumUtil(n - 1, k, arr, dp);
-        }
-    }
+    
 }
