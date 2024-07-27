b5ea27cc3a843a9b0e192735ee0d67b83ae98713
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-07-27 14:10:02 +0530

    Time: 24 ms (34.92%) | Memory: 50.4 MB (86.13%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,5 +1,7 @@
 class Solution {
 
+
+    //Memoization Technique
     public static int lcs(int ind1, int ind2, String t1, String t2, int[][] dp){
         if(ind1 <0 || ind2 <0) return 0;
 
@@ -25,8 +27,11 @@ class Solution {
         return lcs(text1.length()-1,text2.length()-1,text1,text2,dp);
 
     }
+}
 
-    // public static int lcs(String a, String b, int dp[][]){
+
+    //Tabulation
+    // public static int lcs2(String a, String b, int dp[][]){
     
     //     for(int i=0;i<=a.length();i++){
     //         for(int j =0 ;j<=b.length();j++){
@@ -44,4 +49,24 @@ class Solution {
 
     //     return dp[a.length()][b.length()];
     // }
-}
+
+
+//     public static int lcs2(int i, int j, String s1, String s2, int[][] dp){
+//         if(i==0 || j==0) dp[i][j] = 0;
+//         for(int j=0;j<m-1;j++) dp[0][j] = 0;
+//         for(int i= 0;i<n-1;i++) dp[i][0] = 0;
+
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=n;j++){
+//                 if(s1.charAt(i)==s2.charAt(j)){
+//                     return dp[i][j] = 1+dp[i-1][j-1];
+//                 }
+//                 else{
+//                     return dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+//                 }
+//             }
+//         }
+
+//         return dp[n][m];
+//     }
+// }
