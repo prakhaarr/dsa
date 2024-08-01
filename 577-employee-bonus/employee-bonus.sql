ad53fbbd1dd6020154844221b08313b36ccb4d75
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-08-02 00:20:19 +0530

    Time: 867 ms (67.78%) | Memory: 0B (100.00%) - LeetSync

---
+++ 577-employee-bonus/employee-bonus.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+#jisko bonus nahi mila and jisko 1000 se kam mila 
+
+
+select e.name,b.bonus from Employee e left join Bonus b on e.empId = b.empId where b.bonus<1000  OR b.bonus IS NULL;
