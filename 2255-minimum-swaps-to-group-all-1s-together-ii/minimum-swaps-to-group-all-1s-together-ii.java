bc19bc5c0207f5e2008d0873f23669c5c84c7421
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-08-03 12:54:27 +0530

    Time: 10 ms (47.21%) | Memory: 61.4 MB (24.87%) - LeetSync

--- 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.java
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.java
@@ -1,36 +1,94 @@
 class Solution {
     public int minSwaps(int[] nums) {
-        int total = 0;
-        for (int num : nums) {
-            total += num; // Total number of 1's in the array
-        }
+
         int n = nums.length;
-        
-        // If there are no 1's, no swaps are needed
-        if (total == 0) {
-            return 0;
+
+        int[] temp = new int[2*n];
+
+        for(int i =0;i<2*n;i++){
+            temp[i] = nums[i%n];
         }
-        
-        // Extend the array to handle the circular nature
-        int[] extendedNums = new int[n * 2];
-        System.arraycopy(nums, 0, extendedNums, 0, n);
-        System.arraycopy(nums, 0, extendedNums, n, n);
-        
-        // Initialize the sliding window
-        int windowSum = 0;
-        for (int i = 0; i < total; i++) {
-            windowSum += extendedNums[i];
+
+        int totalOnes = 0;
+        for(int i =0;i<n;i++){
+            if(nums[i]==1){
+                totalOnes++;
+            }
         }
-        int maxOnes = windowSum;
-        int swaps = total - maxOnes;
-        
-        for (int i = 1; i < n; i++) {
-            // Update the number of 1's in the current window by sliding the window
-            windowSum += extendedNums[i + total - 1] - extendedNums[i - 1];
-            maxOnes = Math.max(maxOnes, windowSum);
-            swaps = Math.min(swaps, total - maxOnes);
+
+        int i =0;
+        int j =0;
+        int currOnes =0;
+        int maxCount =0;
+
+
+        while(j<2*n){
+            if(temp[j]==1){
+                currOnes++;
+    
+            }
+
+            if(j-i+1 > totalOnes){
+                currOnes -= temp[i];
+                i++;
+            }
+            maxCount = Math.max(maxCount,currOnes);
+            j++;
         }
+
+        return totalOnes - maxCount;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // int total = 0;
+        // for (int num : nums) {
+        //     total += num; // Total number of 1's in the array
+        // }
+        // int n = nums.length;
         
-        return swaps;
+        // // If there are no 1's, no swaps are needed
+        // if (total == 0) {
+        //     return 0;
+        // }
+        
+        // // Extend the array to handle the circular nature
+        // int[] extendedNums = new int[n * 2];
+        // System.arraycopy(nums, 0, extendedNums, 0, n);
+        // System.arraycopy(nums, 0, extendedNums, n, n);
+        
+        // // Initialize the sliding window
+        // int windowSum = 0;
+        // for (int i = 0; i < total; i++) {
+        //     windowSum += extendedNums[i];
+        // }
+        // int maxOnes = windowSum;
+        // int swaps = total - maxOnes;
+        
+        // for (int i = 1; i < n; i++) {
+        //     // Update the number of 1's in the current window by sliding the window
+        //     windowSum += extendedNums[i + total - 1] - extendedNums[i - 1];
+        //     maxOnes = Math.max(maxOnes, windowSum);
+        //     swaps = Math.min(swaps, total - maxOnes);
+        // }
+        
+        // return swaps;
     }
 }
