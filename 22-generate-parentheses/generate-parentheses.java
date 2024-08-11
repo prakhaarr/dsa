d49ae5b1c2a2d4a5af9d4a10f776548679bce3a5
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-08-11 18:05:42 +0530

    Time: 1 ms (86.13%) | Memory: 43.4 MB (22.41%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,23 +1,22 @@
 class Solution {
 
-    public static void helper(List<String> ans , int n,int open, int close,String curr){
-        if(curr.length()==n*2){
+    private void helper(List<String>ans, int n , int open, int close, String curr){
+        if(curr.length()==2*n){
             ans.add(curr);
             return;
         }
-
-        if(open<n){
-            helper(ans,n,open+1,close,curr + "(");
+        if(open <n){
+            helper(ans,n,open+1,close,curr+"(");
         }
         if(close<open){
-            helper(ans,n,open,close+1,curr + ")");
+            helper(ans,n,open,close+1,curr+")");
         }
-
     }
+
     public List<String> generateParenthesis(int n) {
-        List<String> ans = new ArrayList<>();
-        helper(ans,n,0,0,"");
-        System.gc();
-        return ans;
+        List<String> ls = new ArrayList<>();
+        String temp = new String();
+        helper(ls,n,0,0,temp);
+        return ls;
     }
 }
