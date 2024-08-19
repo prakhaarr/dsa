608225b7e8b4bccf7b802c924f5832d33217deb2
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-08-20 01:26:22 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (19.18%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,25 +1,20 @@
 class Solution {
-    
-    private void rec(int[] nums, int index, List<Integer> current, List<List<Integer>> result) {
-        if (index == nums.length) {
-            result.add(new ArrayList<>(current));
+    public static void helper(List<Integer> ls, int i,List<List<Integer>> ans,int[] nums){
+        if(i==nums.length){
+            ans.add(new ArrayList<>(ls));
             return;
         }
-        
-        // Including the current number in the subset
-        current.add(nums[index]);
-        rec(nums, index + 1, current, result);
-        
-        // Excluding the current number from the subset
-        current.remove(current.size() - 1);
-        rec(nums, index + 1, current, result);
+
+        ls.add(nums[i]);
+        helper(ls,i+1,ans,nums);
+
+        ls.remove(ls.size()-1);
+        helper(ls,i+1,ans,nums);
     }
-    
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> current = new ArrayList<>();
-        rec(nums, 0, current, result);
-        return result;
-        
+        List<List<Integer>> ans = new ArrayList<>();
+        helper(new ArrayList<>(),0,ans,nums);
+
+        return ans;
     }
 }
