afc590ee6c203100535cf1ec19abbe323dc299ce
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-08-23 18:05:43 +0530

    Time: 2 ms (97.18%) | Memory: 44.2 MB (68.51%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -13,24 +13,56 @@
  *     }
  * }
  */
+// class Solution {
+//     public String preOrder(TreeNode root){
+//         if(root==null){
+//             return "null";
+//         }
+
+//         StringBuilder sb = new StringBuilder("^");
+//         sb.append(root.val);
+//         sb.append(preOrder(root.left));
+//         sb.append(preOrder(root.right));
+
+//         return sb.toString();
+
+//     }
+//     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+//         String fullTree = preOrder(root);
+//         String subTree = preOrder(subRoot);
+
+//         return (fullTree.contains(subTree));
+//     }
+// }
 class Solution {
-    public String preOrder(TreeNode root){
-        if(root==null){
-            return "null";
+    public boolean isIdentical(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null) {
+            return true;
+        }
+        if (root == null || subRoot == null) {
+            return false;
+        }
+        if (root.val == subRoot.val) {
+            return isIdentical(root.left, subRoot.left) && isIdentical(root.right, subRoot.right);
+        }
+        return false;
+    }
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) {
+            return false;
         }
 
-        StringBuilder sb = new StringBuilder("^");
-        sb.append(root.val);
-        sb.append(preOrder(root.left));
-        sb.append(preOrder(root.right));
+        if (subRoot == null) {
+            return true;
+        }
 
-        return sb.toString();
+        if (root.val == subRoot.val) {
+            if (isIdentical(root, subRoot)) {
+                return true;
+            }
+        }
 
-    }
-    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        String fullTree = preOrder(root);
-        String subTree = preOrder(subRoot);
-
-        return (fullTree.contains(subTree));
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
 }
