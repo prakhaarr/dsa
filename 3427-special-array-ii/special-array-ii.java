6d5697e6708a6ed6297ecc0fb7aa24afe498a774
prakhaarr <122115866+prakhaarr@users.noreply.github.com>
2024-12-09 23:05:50 +0530

    Time: 3 ms (86.83%) | Memory: 98.7 MB (44.79%) - LeetSync

---
+++ 3427-special-array-ii/special-array-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean[] isArraySpecial(int[] nums, int[][] queries) {
+        int n=nums.length;
+        int m=queries.length;
+        boolean[] ans=new boolean[m];
+
+        int[] preSum=new int[n]; //Prefix Sum 
+        int cnt=0;
+        for(int i=1; i<n; i++){
+            //if( (nums[i-1]%2==0 && nums[i]%2==1) || (nums[i-1]%2==1 && nums[i]%2==0) ){ //Checking pair (EVEN & ODD) || (ODD & EVEN)
+            if( nums[i-1]%2 != nums[i]%2 ){
+                cnt++;
+            }
+            preSum[i]=cnt;
+        }
+
+
+        for(int i=0; i<m; i++){
+            ans[i]=true;           
+            if(preSum[queries[i][1]]-preSum[queries[i][0]] != queries[i][1]-queries[i][0] ){ 
+                ans[i]=false;                                
+            }            
+        }
+
+        return ans;
+    }
+
+}
